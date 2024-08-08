bc7d6d0f7085278752e3a3027d9c551dd6b0bcc8
Manuel Cattelan <cattelanmanuel07@gmail.com>
2024-08-08 15:29:49 +0200

    feat(scripts): battery monitoring script

--- .config/i3/scripts/autostart.sh
+++ .config/i3/scripts/autostart.sh
@@ -2,3 +2,4 @@
 
 source "$HOME/.config/i3/scripts/configure_monitors.sh"
 source "$HOME/.config/i3/scripts/configure_background.sh"
+source "$HOME/.config/i3/scripts/battery_monitor.sh"
---
+++ .config/i3/scripts/battery_monitor.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+last_received_notification=""
+while true; do
+    battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+
+    if [ $battery_capacity -le 20 ] && [ $battery_capacity -gt 10 ] && [ "$last_notification" != "low" ]; then
+        dunstify -u critical -h string:x-dunst-stack-tag:battery \
+            "Low battery!" "The battery capacity is now at $battery_capacity%." && \
+            last_received_notification="low"
+
+    elif [ $battery_capacity -le 10 ] && [ $battery_capacity -gt 5 ] && [ "$last_notification" != "very low" ]; then
+        dunstify -u critical -h string:x-dunst-stack-tag:battery \
+            "Very low battery!" "The battery capacity is now at $battery_capacity%." && \
+            last_received_notification="very low"
+
+    elif [ $battery_capacity -le 5 ] && [ "$last_notification" != "critical" ]; then
+        dunstify -u critical -h string:x-dunst-stack-tag:battery \
+            "Critically low battery!" "The battery capacity is now at $battery_capacity%." && \
+            last_received_notification="critical"
+    fi
+
+    sleep 60
+done
+
