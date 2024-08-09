318c9542fcd751e33df69d5aae5f343d0fe6e59c
Manuel Cattelan <cattelanmanuel07@gmail.com>
2024-08-09 10:51:53 +0200

    fix(scripts): only trigger battery notification if not charging

--- .config/i3/scripts/battery_monitor.sh
+++ .config/i3/scripts/battery_monitor.sh
@@ -2,22 +2,25 @@
 
 last_received_notification=""
 while true; do
+    battery_status=$(cat /sys/class/power_supply/BAT0/status)
     battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 
-    if [ $battery_capacity -le 20 ] && [ $battery_capacity -gt 10 ] && [ "$last_notification" != "low" ]; then
-        dunstify -u critical -h string:x-dunst-stack-tag:battery \
-            "Low battery!" "The battery capacity is now at $battery_capacity%." && \
-            last_received_notification="low"
+    if [ $battery_status != "Charging" ]; then
+        if [ $battery_capacity -le 20 ] && [ $battery_capacity -gt 10 ] && [ "$last_received_notification" != "low" ]; then
+            dunstify -u critical -h string:x-dunst-stack-tag:battery \
+                "Low battery!" "The battery capacity is now at $battery_capacity%." && \
+                last_received_notification="low"
 
-    elif [ $battery_capacity -le 10 ] && [ $battery_capacity -gt 5 ] && [ "$last_notification" != "very low" ]; then
-        dunstify -u critical -h string:x-dunst-stack-tag:battery \
-            "Very low battery!" "The battery capacity is now at $battery_capacity%." && \
-            last_received_notification="very low"
+        elif [ $battery_capacity -le 10 ] && [ $battery_capacity -gt 5 ] && [ "$last_received_notification" != "very low" ]; then
+            dunstify -u critical -h string:x-dunst-stack-tag:battery \
+                "Very low battery!" "The battery capacity is now at $battery_capacity%." && \
+                last_received_notification="very low"
 
-    elif [ $battery_capacity -le 5 ] && [ "$last_notification" != "critical" ]; then
-        dunstify -u critical -h string:x-dunst-stack-tag:battery \
-            "Critically low battery!" "The battery capacity is now at $battery_capacity%." && \
-            last_received_notification="critical"
+        elif [ $battery_capacity -le 5 ] && [ "$last_received_notification" != "critical" ]; then
+            dunstify -u critical -h string:x-dunst-stack-tag:battery \
+                "Critically low battery!" "The battery capacity is now at $battery_capacity%." && \
+                last_received_notification="critical"
+        fi
     fi
 
     sleep 60
