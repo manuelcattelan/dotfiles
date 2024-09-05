a61f80ec464e2334d6667283c091eddc76b35700
Manuel Cattelan <cattelanmanuel07@gmail.com>
2024-09-05 07:54:35 +0200

    revert(tsserver): organize import autocommand as it messes with indentation

--- .config/nvim/lua/mcattelan/core/autocmds.lua
+++ .config/nvim/lua/mcattelan/core/autocmds.lua
@@ -1,12 +1,5 @@
 local utils = require("mcattelan.utils")
 
-local function organize_imports()
-  vim.lsp.buf.execute_command({
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-  })
-end
-
 -- Add custom autocmd that highlights text region after
 -- yanking (copying) it.
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -15,13 +8,4 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
-})
-
--- Organize imports on save for typescript files by leveraging
--- the Typescript language server.
-vim.api.nvim_create_autocmd("BufWritePre", {
-  desc = "Organize imports on save for typescript files",
-  group = utils.autocmds.nvim_create_augroup("organize-imports"),
-  pattern = "*.ts",
-  callback = organize_imports,
 })
